2c14c98bd725e44bd926a99e263189373e51b67a
Erik <eebbesen@kth.se>
2024-03-30 13:56:17 +0100

    style changes

--- app/src/game/scenes/MainMenu.js
+++ app/src/game/scenes/MainMenu.js
@@ -12,11 +12,11 @@ export class MainMenu extends Scene
 
     create ()
     {
-        this.add.image(512, 384, 'background');
+        this.add.image(960, 384, 'background');
 
-        this.logo = this.add.image(512, 300, 'logo').setDepth(100);
+        this.logo = this.add.image(960, 300, 'logo').setDepth(100);
 
-        this.add.text(512, 460, 'Main Menu', {
+        this.add.text(960, 460, 'Main Menu', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
